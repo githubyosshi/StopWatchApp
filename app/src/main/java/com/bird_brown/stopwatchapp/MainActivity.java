01877d7c63192333f6c03e8eac824325c668cab2
githubyosshi <kanrisya.shisutemu@gmail.com>
2019-08-04 19:00:55 +0900

    Runnable commit

--- app/src/main/java/com/bird_brown/stopwatchapp/MainActivity.java
+++ app/src/main/java/com/bird_brown/stopwatchapp/MainActivity.java
@@ -1,18 +1,26 @@
 package com.bird_brown.stopwatchapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-//import android.support.v7.app.ActionBarActivity;
+//import androidx.appcompat.app.AppCompatActivity;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
 
-    private long startTimer;
+public class MainActivity extends ActionBarActivity {
+
+    private long startTime;
+
+    private Handler handler = new Handler();
+    private Runnable updateTimer;
 
     private Button startButton;
     private Button stopButton;
@@ -32,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 //        startButton.setEnabled(true);
 //        stopButton.setEnabled(false);
 //        resetButton.setEnabled(false);
-        setButtonState(true, false,false)
+        setButtonState(true, false,false);
 
     }
     public void setButtonState(boolean start, boolean stop, boolean reset) {
@@ -42,12 +50,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startTimer(View view) {
-        // startTimerの取得
-        startTimer = SystemClok.elapsedRealtime();  // 起動してからの経過時間（ミリ秒）
+        // startTimeの取得
+        startTime = SystemClock.elapsedRealtime(); // 起動してからの経過時間（ミリ秒）
         // 一定時間ごとに現在の経過時間を表示
+        // Handler -> Runnable(処理) -> UI
+        updateTimer = new Runnable() {
+            @Override
+            public void run() {
+                long t = SystemClock.elapsedRealtime() - startTime; // ミリ秒
+                SimpleDateFormat sdf = new SimpleDateFormat("mm:ss.SSS", Locale.US);
+                timerLabel.setText(sdf.format(t));
+            }
+        };
 
         // ボタンの操作
-        setButtonState(false,true,false)
+        setButtonState(false,true,false);
 
     }
 }
