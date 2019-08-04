c0ce2f00381dd13621654954b777e4364c26b56b
githubyosshi <kanrisya.shisutemu@gmail.com>
2019-08-04 15:31:25 +0900

    Flow of processing commit

--- app/src/main/java/com/bird_brown/stopwatchapp/MainActivity.java
+++ app/src/main/java/com/bird_brown/stopwatchapp/MainActivity.java
@@ -7,14 +7,17 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private long startTimer;
+
     private Button startButton;
     private Button stopButton;
     private Button resetButton;
-
+    private TextView timerLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         startButton = (Button) findViewById(R.id.startButton);
         stopButton = (Button) findViewById(R.id.stopButton);
         resetButton = (Button) findViewById(R.id.resetButton);
+        timerLabel = (TextView) findViewById(R.id.timerLabel);
 
 //        startButton.setEnabled(true);
 //        stopButton.setEnabled(false);
@@ -35,5 +39,15 @@ public class MainActivity extends AppCompatActivity {
         startButton.setEnabled(start);
         stopButton.setEnabled(stop);
         resetButton.setEnabled(reset);
+    }
+
+    public void startTimer(View view) {
+        // startTimerの取得
+        startTimer = SystemClok.elapsedRealtime();  // 起動してからの経過時間（ミリ秒）
+        // 一定時間ごとに現在の経過時間を表示
+
+        // ボタンの操作
+        setButtonState(false,true,false)
+
     }
 }
